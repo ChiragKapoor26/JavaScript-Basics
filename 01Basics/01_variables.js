71b4e97b2269aaa05eaa55e6e660231d6351f41d
Chirag Kapoor <chiragkapoor80@gmail.com>
2024-09-30 05:02:08 +0000

    Understanding Variables

---
+++ 01Basics/01_variables.js
@@ -0,0 +1,15 @@
+// In this file we Understand about the Story of let var and const variables of JavaScript
+const accountId = 1234;
+let accountName = 'Chirag';
+let accountPassword = '2005';
+var accountAddress = 'Krishna Nagar Jalandhar';
+accountCity = 'Bharatpur';
+//accountId = 5678; // This is Not Allowed
+console.table([accountId,accountName,accountPassword,accountAddress,accountCity]);
+/*
+The key Difference between the var and let datatypes is as follows:-
+the var is functional scope variable and let is Block Scope Variable
+
+Matlab Agar kise ke aage let likha hai toh uski value Change Bhi ho Sakti hai or Vo ek block scope
+matlab {}iske andar hi defined hai agar var assigned hai toh vo puri file mai globally defined hai
+*/
